79f320bc6cee06fbe29e6bdf742360c3b1fb8cca
seannxh <nohsean6@gmail.com>
2025-01-03 19:15:48 -0600

    F

--- banking/settings.py
+++ banking/settings.py
@@ -24,6 +24,8 @@ print(os.environ.get('DATABASE_URL'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
