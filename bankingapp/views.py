d6fa3601aa033c041ac3fadd0b23022479187c74
seannxh <nohsean6@gmail.com>
2025-01-03 22:46:50 -0600

    F

--- bankingapp/views.py
+++ bankingapp/views.py
@@ -38,11 +38,16 @@ class SignUpView(APIView):
 
 
 class SignOutView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         try:
             refresh_token = request.data.get("refresh_token")
+            if not refresh_token:
+                return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
+
             token = RefreshToken(refresh_token)
-            token.blacklist()  # Invalidate the refresh token
+            token.blacklist()
             return Response({"message": "Signout successful"}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
