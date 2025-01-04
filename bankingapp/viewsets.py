c904a9be688428ccc89eb1b35225d92b19f0774b
seannxh <nohsean6@gmail.com>
2025-01-03 19:12:58 -0600

    F

--- bankingapp/viewsets.py
+++ bankingapp/viewsets.py
@@ -15,3 +15,9 @@ class BankAccountViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        account_id = self.request.query_params.get('account_id')
+        if account_id:
+            return Transaction.objects.filter(account_id=account_id)
+        return super().get_queryset()
