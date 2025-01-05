cd25889317f0b7afde11f6a7bccd654cd815864b
seannxh <nohsean6@gmail.com>
2025-01-05 02:51:58 -0600

    F

--- bankingapp/viewsets.py
+++ bankingapp/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Customer, BankAccount, Transaction
 from .serializers import CustomerSerializer, BankAccountSerializer, TransactionSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
@@ -15,13 +16,15 @@ class BankAccountViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        account_id = self.kwargs.get('account_id')
-        account_number = self.kwargs.get('account_number')
-
-        if account_id:
-            return Transaction.objects.filter(account__id=account_id)
-        elif account_number:
-            return Transaction.objects.filter(account__account_number=account_number)
+        account_number = self.kwargs.get('accountNumber')  # Fetch accountNumber from URL
+        if account_number:
+            # Fetch transactions where the account is either the sender or the receiver
+            return Transaction.objects.filter(
+                sender__account_number=account_number
+            ) | Transaction.objects.filter(
+                receiver__account_number=account_number
+            )
         return super().get_queryset()
