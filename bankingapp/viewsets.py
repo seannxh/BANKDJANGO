74d054c53ff6a2c623cb6768180ef36b96f9bbf0
seannxh <nohsean6@gmail.com>
2025-01-09 01:00:53 -0600

    ..

--- bankingapp/viewsets.py
+++ bankingapp/viewsets.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from .models import Customer, BankAccount, Transaction
 from .serializers import CustomerSerializer, BankAccountSerializer, TransactionSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from django.db.models import Q
 
 
@@ -17,13 +17,14 @@ class BankAccountViewSet(viewsets.ModelViewSet):
 
 class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         account_number = self.kwargs.get('accountNumber')
+        print(f"Fetching transactions for account: {account_number}")  # Debug
         if account_number:
             return Transaction.objects.filter(
                 Q(sender__account_number=account_number) |
                 Q(receiver__account_number=account_number)
-            ).select_related('sender', 'receiver').order_by('-timestamp')
+            ).select_related('sender', 'receiver').order_by('-created_at')
         return Transaction.objects.none()
